9293181791fda27afc6633c20c2efe11b1944931
tdouce <travisdouce@gmail.com>
2014-03-31 19:45:18 -0400

    Adds RemoteDatabaseCleaner

--- lib/remote_database_cleaner.rb
+++ lib/remote_database_cleaner.rb
@@ -1,5 +1,34 @@
 require "remote_database_cleaner/version"
+require 'remote_database_cleaner/config'
+require 'remote_database_cleaner/http'
+require 'remote_database_cleaner/config_struct'
 
 module RemoteDatabaseCleaner
-  # Your code goes here...
+  class RemoteDatabaseCleaner
+    def params 
+      { :database => :clean }
+    end
+  end
+
+  def self.configure(opts = { :config_struct => ConfigStruct, :config => Config }, &block)
+    config      = opts.fetch(:config_struct).block_to_hash(block)
+    self.config = opts.fetch(:config).configure(config)
+  end
+
+  def self.clean(http = Http)
+    database_cleaner  = RemoteDatabaseCleaner.new
+    http.post(config, database_cleaner.params)
+  end
+
+  def self.config
+    @config
+  end
+
+  def self.config=(config)
+    @config = config
+  end
+
+  def self.reset(config = Config.new)
+    self.config = config
+  end
 end
--- spec/models/remoted_database_cleaner_spec.rb
+++ spec/models/remoted_database_cleaner_spec.rb
@@ -1,8 +1,39 @@
-require 'spec_helper'
+require 'remote_database_cleaner'
 
 describe RemoteDatabaseCleaner do
-
-  it "should work" do
+  it 'should return params for http request' do
+    rfg = RemoteDatabaseCleaner::RemoteDatabaseCleaner.new
+    expect(rfg.params).to eq({ :database => :clean })
   end
 
+  it 'should be able to configure with a block' do
+    pending
+  end
+
+  describe '.config' do
+    it 'should be able to set and get config' do
+      config = double('config')
+      RemoteDatabaseCleaner.config = config
+      expect(RemoteDatabaseCleaner.config).to equal(config)
+    end
+  end
+
+  describe '.reset' do
+    it 'should be able to reset the configuration' do
+      config = double('config')
+      RemoteDatabaseCleaner.config = config
+      RemoteDatabaseCleaner.reset(double('config'))
+      expect(RemoteDatabaseCleaner.config).to_not equal(config)
+    end
+  end
+
+  describe '.clean' do
+    it 'should send http request and parse request' do
+      config = double('config', :home_url => 'http://somewhere', :to_hash => {})
+      http   = double('RemoteDatabaseCleanerFriends::Http', :post => {})
+      RemoteDatabaseCleaner.config = config
+      expect(http).to receive(:post).with(config, { :database => :clean })
+      RemoteDatabaseCleaner.clean(http)
+    end
+  end
 end
